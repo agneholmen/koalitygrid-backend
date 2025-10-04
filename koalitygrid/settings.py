50faaf07a5fd9d40681db95c4487b1aea76ba433
Emil Agneholm <emil.agneholm@gmail.com>
2025-10-04 18:14:15 +0200

    Changed DB to PostgreSQL.

--- koalitygrid/settings.py
+++ koalitygrid/settings.py
@@ -77,8 +77,15 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='prefer'),
+        },
     }
 }
 
